c629b822d7bf19b562a05ec01d86753eef115a29
farohan <68710283+farohan@users.noreply.github.com>
2022-07-15 14:28:20 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('height').innerHTML = `Screen Height: ${screen.height}`;
+document.getElementById('width').innerHTML = `Screen Width: ${screen.width}`;
+document.getElementById('color-depth').innerHTML = `Screen Color Depth: ${screen.colorDepth}`;
+document.getElementById('language').innerHTML = `Languages: ${navigator.languages}`;
+document.getElementById('is-online').innerHTML = `Is online: ${navigator.onLine}`;
+document.getElementById('user-agent').innerHTML = `User agent: ${navigator.userAgent}`;
+document.getElementById('history-length').innerHTML = `History length: ${history.length}`;
